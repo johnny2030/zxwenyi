830102b91817ff7947aae8fef01113b72388ca63
李棒 <347558228@qq.com>
2018-11-18 05:05:49 +0800

    demo制作

--- application/Portal/Controller/UserController.class.php
+++ application/Portal/Controller/UserController.class.php
@@ -46,8 +46,12 @@ class UserController extends HomebaseController {
         if ( IS_POST ) {
             $id = (int)session('login_id');
             $_POST['update_time'] = date('Y-m-d H:i:s',time());
-            $this->common_user_model->where(array('id' => $id))->save($_POST);
-            $this->ajaxReturn(1);
+            $result = $this->common_user_model->where(array('id' => $id))->save($_POST);
+            if ($result){
+                $this->ajaxReturn(1);
+            }else{
+                $this->ajaxReturn(0);
+            }
         }else{
             $id = (int)session('login_id');
             $where = array();
@@ -80,10 +84,21 @@ class UserController extends HomebaseController {
     }
     //医生个人中心
     public function info_doctor() {
-        $id = (int)session('login_id');
-        $doctor = $this->common_user_model->alias('u')->field('u.*,h.name as hospital_n,o.name as office_n,t.name as tag_n')->join('__COMMON_HOSPITAL__ h ON u.hospital=h.id')->join('__COMMON_OFFICE__ o ON u.office=o.id')->join('__COMMON_TAG__ t ON u.tag=t.id')->where(array('u.id' => $id))->find();
-        $this->assign( 'doctor', $doctor );
-        $this->display('../Tieqiao/info_doctor');
+        if ( IS_POST ) {
+            $id = (int)session('login_id');
+            $_POST['update_time'] = date('Y-m-d H:i:s',time());
+            $this->common_user_model->where(array('id' => $id))->save($_POST);
+            $this->ajaxReturn(1);
+        }else{
+            $id = (int)session('login_id');
+            $doctor = $this->common_user_model->where(array('id' => $id))->find();
+            $where = array();
+            $where['del_flg'] = array('eq',0);
+            $list = $this->common_office_model->where($where)->select();
+            $this->assign( 'list', $list );
+            $this->assign( 'doctor', $doctor );
+            $this->display('../Tieqiao/info_doctor');
+        }
     }
     //科室展示
     public function office_preview() {
